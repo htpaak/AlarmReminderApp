198eeda664c94e9a560a955ed8e6a11b61993756
htpaak <htpaak@gmail.com>
2025-04-28 20:22:11 +0900

    fix: UI 동작 수정 (사운드 선택 취소 및 창 중앙 정렬)

--- ui.py
+++ ui.py
@@ -275,7 +275,7 @@ class AlarmApp(QWidget):
         self.setLayout(main_layout)
         self.setWindowTitle('Alarm Reminder App')
         self.setWindowIcon(QIcon('assets/icon.svg')) # 윈도우 아이콘 설정
-        self.setGeometry(300, 300, 600, 400) # 창 크기 및 위치 설정
+        # self.setGeometry(300, 300, 600, 400) # 창 크기 및 위치 설정 (center() 호출 후 실행되어 중앙 정렬 무시함)
 
     def center(self):
         """창을 화면 중앙으로 이동시킵니다."""
